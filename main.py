8917c2ba2ac85fc936b7981529b5503ac53a9451
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-05-26 21:41:27 -0400

    completed 11 patterns

---
+++ main.py
@@ -0,0 +1,92 @@
+def pattern1(n):
+    for i in range(n):
+        for j in range(n):
+            print("*", end=" ")
+        print("\n")
+
+
+def pattern2(n):
+    for i in range(n):
+        for j in range(i):
+            print("*", end=" ")
+        print("\n")
+
+
+def pattern3(n):
+    for i in range(1, n + 1):
+        for j in range(1, i):
+            print(j, end=" ")
+        print("\n")
+
+
+def pattern4(n):
+    for i in range(1, n + 1):
+        for j in range(i):
+            print(i, end=" ")
+        print("\n")
+
+
+def pattern5(n):
+    for i in reversed(range(1, n + 1)):
+        for j in range(i):
+            print("*", end=" ")
+        print("\n")
+
+
+def pattern6(n):
+    for i in reversed(range(1, n + 1)):
+        for j in range(1, i):
+            print(j, end=" ")
+        print("\n")
+
+
+def pattern7(n):
+    for i in range(n):
+        for _ in range(n - i - 1):
+            print(" ", end=" ")
+        for j in range((2 * i) + 1):
+            print("*", end=" ")
+        print(" ")
+
+
+def pattern8(n):
+    for i in range(n):
+        for _ in range(i):
+            print(" ", end=" ")
+        for j in range(2 * n - 2 * i - 1):
+            print("*", end=" ")
+        print(" ")
+
+
+def pattern9():
+    pattern7(5)
+    pattern8(5)
+
+
+def pattern10(n):
+    for i in range(n):
+        for j in range(i):
+            print("*", end=" ")
+        print("\n")
+    for i in reversed(range(n)):
+        for j in range(i):
+            print("*", end=" ")
+        print("\n")
+
+
+# Floyd's Triangle
+def pattern11(n):
+    for i in range(n):
+        if (i%2 == 0):
+            start = 1
+        else:
+            start = 0
+        for j in range(i):
+
+            print(start, end=" ")
+            start = 1 - start
+        print("\n")
+
+
+
+pattern11(5)
